4844ffb9b00ba67c891418ed9acdd5e117e6f36d
Liviu Ionescu <ilg@livius.net>
2024-01-28 19:39:37 +0200

    trigger*.sh: -x

--- scripts/trigger-preview-build.sh
+++ scripts/trigger-preview-build.sh
@@ -1,10 +1,11 @@
 set -e
+set -x
 
 curl \
 --fail-with-body  \
--sSL\
+-sSL \
 https://api.github.com/repos/cronica-it/preview/actions/workflows/publish-github-pages.yml/dispatches \
--H "Authorization: $1" \
+-H "Authorization: Bearer $1" \
 -H 'Accept: application/vnd.github.v3+json' \
 -H 'Content-Type: application/json' \
 --data '{"ref\":\"master\",\"inputs\":{}}'
