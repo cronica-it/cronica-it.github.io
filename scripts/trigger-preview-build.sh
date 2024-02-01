993942d99208628e28291eabedadc503e83fcfa5
Liviu Ionescu <ilg@livius.net>
2024-02-01 09:30:27 +0200

    trigger-preview-build.sh update

--- scripts/trigger-preview-build.sh
+++ scripts/trigger-preview-build.sh
@@ -1,11 +1,51 @@
-set -e
-set -x
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+token="$1"
+organization="cronica-it"
+repo="preview"
+action="publish-github-pages.yml"
 
 curl \
 --fail-with-body  \
 -sSL \
-https://api.github.com/repos/cronica-it/preview/actions/workflows/publish-github-pages.yml/dispatches \
--H "Authorization: Bearer $1" \
+https://api.github.com/repos/${organization}/${repo}/actions/workflows/${action}/dispatches \
+-H "Authorization: Bearer ${token}" \
 -H 'Accept: application/vnd.github.v3+json' \
 -H 'Content-Type: application/json' \
 --data '{"ref":"master","inputs":{}}'
+
+# -----------------------------------------------------------------------------
