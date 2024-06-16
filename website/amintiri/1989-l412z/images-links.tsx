b322f057b5a67500adaf3b1da02ceb64d50dfa96
Liviu Ionescu <ilg@livius.net>
2024-06-16 13:25:52 +0300

    add 1989-l412z

---
+++ website/amintiri/1989-l412z/images-links.tsx
@@ -0,0 +1,26 @@
+export const imagesLinks = [
+  {
+      src: "https://cronica-it.github.io/imagini/1989/l412z/board-top.jpg",
+      alt: "board-top.jpg"
+  },
+  {
+    src: "https://cronica-it.github.io/imagini/1989/l412z/board-bottom.jpg",
+    alt: "board-bottom.jpg"
+  },
+  {
+    src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-1.jpg",
+    alt: "encoder-1.jpg"
+  },
+  {
+    src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-2.jpg",
+    alt: "encoder-2.jpg"
+  },
+  {
+    src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-3.jpg",
+    alt: "encoder-3.jpg"
+  },
+  {
+    src: "https://cronica-it.github.io/imagini/1989/l412z/transceiverul-lui-bord.jpg",
+    alt: "transceiverul-lui-bord.jpg"
+  }
+]
