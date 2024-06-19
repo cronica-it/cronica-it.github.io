2b33312bd831795e448df5b50e4f2977e71385b3
Liviu Ionescu <ilg@livius.net>
2024-06-19 13:28:01 +0300

    l412z update small images in table

--- website/amintiri/1989-l412z/images-links.tsx
+++ website/amintiri/1989-l412z/images-links.tsx
@@ -1,34 +1,42 @@
 export const imagesLinks = [
   {
       src: "https://cronica-it.github.io/imagini/1989/l412z/board-top.jpg",
+      srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/board-top-small.jpg",
       alt: "board-top.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/board-bottom.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/board-bottom-small.jpg",
     alt: "board-bottom.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/board-all.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/board-all-small.jpg",
     alt: "board-all.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-1.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/encoder-1-small.jpg",
     alt: "encoder-1.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-2.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/encoder-2-small.jpg",
     alt: "encoder-2.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/encoder-3.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/encoder-3-small.jpg",
     alt: "encoder-3.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/transceiverul-lui-bord.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/transceiverul-lui-bord-small.jpg",
     alt: "transceiverul-lui-bord.jpg"
   },
   {
     src: "https://cronica-it.github.io/imagini/1989/l412z/transceiverul-lui-bord-perspectiva.jpg",
+    srcSmall: "https://cronica-it.github.io/imagini/1989/l412z/transceiverul-lui-bord-perspectiva-small.jpg",
     alt: "transceiverul-lui-bord-perspectiva.jpg"
   }
 ]
